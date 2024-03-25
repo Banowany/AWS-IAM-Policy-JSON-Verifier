6dc6d3caf391a6f4f3c25dcba6d22372ea2b035e
“Krzysztof <krzysztof.gebka2001@gmail.com>
2024-03-25 16:46:13 +0100

    Implement json of IAMPolicy verifier

--- src/main/java/org/example/JSONOfIAMPolicyVerifier.java
+++ src/main/java/org/example/JSONOfIAMPolicyVerifier.java
@@ -1,9 +1,37 @@
 package org.example;
 
+import org.json.JSONObject;
+
+//throw JsonException if iamPolicy is not a json
 public class JSONOfIAMPolicyVerifier implements IAMPolicyVerifier {
-    //TODO: Implement
     @Override
     public boolean verify(String iamPolicy) {
-        return false;
+        JSONObject jsonObject = new JSONObject(iamPolicy);
+
+        if (jsonObject.has("PolicyDocument")) {
+            var policyDocumentObject = jsonObject.getJSONObject("PolicyDocument");
+
+            if (policyDocumentObject.has("Statement")) {
+                var statementArray = policyDocumentObject.getJSONArray("Statement");
+
+                for (int i = 0; i < statementArray.length(); i++) {
+                    var statementObject = statementArray.getJSONObject(i);
+
+                    if (statementObject.has("Resource")) {
+                        var resource = statementObject.getString("Resource");
+
+                        // Based on example I assume that
+                        // "Method shall return logical false if
+                        // an input JSON Resource field contains a single asterisk"
+                        // means equality with "*"
+                        if (resource.equals("*")) {
+                            return false;//main case
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;//other cases
     }
 }
