12e6755b95150224d536317c901c16b8c205e7a3
“Krzysztof <krzysztof.gebka2001@gmail.com>
2024-03-26 08:52:38 +0100

    Finish implementation of verifier

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        var fileReader = new FileReader();
+        var path = args[0];
+        var content = fileReader.read(path);
+        IAMPolicyVerifier iamPolicyVerifier = new JSONOfIAMPolicyVerifier();
+        var verifyResult = iamPolicyVerifier.verify(content);
+        System.out.println("Verify result: " + verifyResult);
     }
 }
